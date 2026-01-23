d2ed811c3bde4fdf7453a4f76b2a59395610af65
Isaac Patrocinio <isaacpatrocinio27@gmail.com>
2026-01-23 15:23:10 -0300

    Service layer and component registration

--- src/main/java/br/com/isaacpatrocinio/ordersystem/resources/UserResource.java
+++ src/main/java/br/com/isaacpatrocinio/ordersystem/resources/UserResource.java
@@ -1,19 +1,35 @@
 package br.com.isaacpatrocinio.ordersystem.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.isaacpatrocinio.ordersystem.entities.User;
+import br.com.isaacpatrocinio.ordersystem.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService userService;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User user = new User(1L, "Maria", "maria@gmail.com", "+5511999853366", "abc12345");
+	public ResponseEntity<List<User>> findAll() {
+		
+		List<User> users = userService.findAll();
+		
+		return ResponseEntity.ok().body(users);
+	} 
+	
+	@GetMapping(value= "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User user = userService.findById(id);
 		return ResponseEntity.ok().body(user);
 	}
 }
