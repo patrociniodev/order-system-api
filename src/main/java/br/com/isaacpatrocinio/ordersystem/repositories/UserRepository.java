d2831201b0e9ece4da68b97313a35dcdacb6e17c
Isaac Patrocinio <isaacpatrocinio27@gmail.com>
2026-01-23 12:59:53 -0300

    JPA repository, dependency injection and database seeding

---
+++ src/main/java/br/com/isaacpatrocinio/ordersystem/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package br.com.isaacpatrocinio.ordersystem.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import br.com.isaacpatrocinio.ordersystem.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+}
