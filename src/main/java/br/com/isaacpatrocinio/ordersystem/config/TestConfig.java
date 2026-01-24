63424aab56cdc6f36ef403b5f31391115ed10b42
Isaac Patrocinio <isaacpatrocinio27@gmail.com>
2026-01-24 20:24:14 -0300

    Add remaining implementation of order entity, instant ISO 8601

--- src/main/java/br/com/isaacpatrocinio/ordersystem/config/TestConfig.java
+++ src/main/java/br/com/isaacpatrocinio/ordersystem/config/TestConfig.java
@@ -1,5 +1,6 @@
 package br.com.isaacpatrocinio.ordersystem.config;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +8,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import br.com.isaacpatrocinio.ordersystem.entities.Order;
 import br.com.isaacpatrocinio.ordersystem.entities.User;
+import br.com.isaacpatrocinio.ordersystem.repositories.OrderRepository;
 import br.com.isaacpatrocinio.ordersystem.repositories.UserRepository;
 
 @Configuration
@@ -16,13 +19,21 @@ public class TestConfig implements CommandLineRunner {
 
 	@Autowired
 	private UserRepository userRepository;
-
+	
+	@Autowired
+	private OrderRepository orderRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
+		//Database seeding
 		User user1 = new User(null, "Maria Brown", "maria@gmail.com", "998888888", "123456");
 		User user2 = new User(null, "Alex Green", "alex@gmail.com", "997777777", "123456");
-		
 		userRepository.saveAll(Arrays.asList(user1, user2));
+
+		Order order1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), user1);
+		Order order2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), user2);
+		Order order3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), user1);
+		orderRepository.saveAll(Arrays.asList(order1, order2, order3));
 	}
 
 }
--- src/main/java/br/com/isaacpatrocinio/ordersystem/resources/OrderResource.java
+++ src/main/java/br/com/isaacpatrocinio/ordersystem/resources/OrderResource.java
@@ -9,25 +9,25 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.isaacpatrocinio.ordersystem.entities.User;
-import br.com.isaacpatrocinio.ordersystem.services.UserService;
+import br.com.isaacpatrocinio.ordersystem.entities.Order;
+import br.com.isaacpatrocinio.ordersystem.services.OrderService;
 
 @RestController
-@RequestMapping(value = "/users")
-public class UserResource {
+@RequestMapping(value = "/orders")
+public class OrderResource {
 
 	@Autowired
-	private UserService userService;
+	private OrderService orderService;
 
 	@GetMapping
-	public ResponseEntity<List<User>> findAll() {
-		List<User> users = userService.findAll();
-		return ResponseEntity.ok().body(users);
+	public ResponseEntity<List<Order>> findAll() {
+		List<Order> orders = orderService.findAll();
+		return ResponseEntity.ok().body(orders);
 	}
 
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<User> findById(@PathVariable Long id) {
-		User user = userService.findById(id);
-		return ResponseEntity.ok().body(user);
+	public ResponseEntity<Order> findById(@PathVariable Long id) {
+		Order order = orderService.findById(id);
+		return ResponseEntity.ok().body(order);
 	}
 }
