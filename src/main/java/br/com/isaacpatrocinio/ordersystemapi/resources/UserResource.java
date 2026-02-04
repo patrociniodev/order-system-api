d5ee28f6b655d34a01f5fca5c97957b27d6c8703
Isaac Patrocinio <isaacpatrocinio27@gmail.com>
2026-02-04 12:59:45 -0300

    Add user insert

--- src/main/java/br/com/isaacpatrocinio/ordersystemapi/resources/UserResource.java
+++ src/main/java/br/com/isaacpatrocinio/ordersystemapi/resources/UserResource.java
@@ -1,13 +1,17 @@
  package br.com.isaacpatrocinio.ordersystemapi.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.isaacpatrocinio.ordersystemapi.entities.User;
 import br.com.isaacpatrocinio.ordersystemapi.services.UserService;
@@ -29,5 +33,16 @@ public class UserResource {
 	public ResponseEntity<User> findById(@PathVariable Long id) {
 		User user = userService.findById(id);
 		return ResponseEntity.ok().body(user);
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj) {
+		obj = userService.insert(obj);
+		URI uri = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(obj.getId())
+				.toUri();
+		return ResponseEntity.created(uri).body(obj);
 	}
 }
